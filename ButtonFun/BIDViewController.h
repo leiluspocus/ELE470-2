8008d4086c449b2a6aa31917a86fa9cc0d6a4e72
Laila Atrmouh <laila.atrmouh@gmail.com>
2012-09-17 15:28:48 -0400

    first commit

---
+++ ButtonFun/BIDViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDViewController.h
+//  ButtonFun
+//
+//  Created by Laïla Atrmouh on 17/09/12.
+//  Copyright (c) 2012 URI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@end
---
+++ ButtonFun/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ButtonFun
+//
+//  Created by Laïla Atrmouh on 17/09/12.
+//  Copyright (c) 2012 URI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BIDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BIDAppDelegate class]));
+    }
+}
